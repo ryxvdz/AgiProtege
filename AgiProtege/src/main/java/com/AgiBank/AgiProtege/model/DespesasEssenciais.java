3adf42b3a9760f17063b032336d230ea336726cc
dev28 <nayani.reis@gmail.com>
2025-09-22 10:45:52 -0300

    Criado a entidade Seguro despesas

---
+++ AgiProtege/src/main/java/com/AgiBank/AgiProtege/model/DespesasEssenciais.java
@@ -0,0 +1,23 @@
+package com.AgiBank.AgiProtege.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DespesasEssenciais {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_seguro_despesa")
+    private Integer idSeguroDespesas;
+
+    private Double gastosMensais;
+}
+
