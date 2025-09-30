535340940b8efd37c7972b01bb85cc81de8c869f
dev30 <viniciuspapa.batista@gmail.com>
2025-09-30 11:26:13 -0300

    seguro de automovel finalizado e parametros finais implementados

--- AgiProtege/src/main/java/com/AgiBank/AgiProtege/repository/AutomovelRepository.java
+++ AgiProtege/src/main/java/com/AgiBank/AgiProtege/repository/AutomovelRepository.java
@@ -3,7 +3,10 @@ package com.AgiBank.AgiProtege.repository;
 import com.AgiBank.AgiProtege.model.Automovel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AutomovelRepository extends JpaRepository<Automovel, UUID> {
+
+    boolean existsByPlaca(String placa);
 }
